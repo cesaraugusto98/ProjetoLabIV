72d157fd950bb7b7cfc3533e4ab53f4bb89e6305
kevin799 <kevinmikio23@gmail.com>
2018-12-07 16:09:23 -0200

    criacao do banco

--- projetolab4/src/main/resources/dbschema/scripts/scriptInitialSQL.sql
+++ projetolab4/src/main/resources/dbschema/scripts/scriptInitialSQL.sql
@@ -1,14 +1,16 @@
+--liquibase formatted sql
+
+--changeset kevin:201810202316
+--comment: Criacao Todas as Tabelas
+
 CREATE USER 'Ecommerce'@'localhost' IDENTIFIED BY 'fatec';
-DROP if exists USER 'Ecommerce'@'localhost';
 
 GRANT ALL PRIVILEGES ON ecommerce.*  TO 'Ecommerce'@'localhost';
 
-DROP if exists DATABASE lab04db;
-
 CREATE DATABASE lab04db;
-
-DROP if exists SCHEMA lab04db;
 
 CREATE SCHEMA lab04db;
 
-USE lab04db;
+--rollback DROP SCHEMA lab04db;
+--rollback DROP USER 'Ecommerce'@'localhost'
+--rollback DROP DATABASE lab04db
