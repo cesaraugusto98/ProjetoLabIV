354ef19f94d0e4ec2f09f020023722cd62b41c00
lucasdlg5 <lucasdomingosleao55@hotmail.com>
2018-12-08 23:34:49 -0200

    Alteracao Classe MovimentoSaida - Criacao Controller e Repository MovimentoSaida

--- projetolab4/src/main/java/com/projetolab4/model/MovimentoSaida.java
+++ projetolab4/src/main/java/com/projetolab4/model/MovimentoSaida.java
@@ -1,8 +1,22 @@
 package com.projetolab4.model;
 
 import javax.persistence.*;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "MES_MOVIMENTO_SAIDA")
 public class MovimentoSaida {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
