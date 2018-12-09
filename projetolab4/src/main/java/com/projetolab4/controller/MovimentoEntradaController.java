8195a2f7f7bbede3579d12cd597f29eab1f3c6ca
lucasdlg5 <lucasdomingosleao55@hotmail.com>
2018-12-08 23:09:46 -0200

    Controller e Repository - MovimentoEntrada

---
+++ projetolab4/src/main/java/com/projetolab4/controller/MovimentoEntradaController.java
@@ -0,0 +1,39 @@
+package com.projetolab4.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.projetolab4.model.MovimentoEntrada;
+import com.projetolab4.repository.MovimentoEntradaRepository;
+
+public class MovimentoEntradaController {
+
+	private MovimentoEntradaRepository repository;
+	
+	MovimentoEntradaController(MovimentoEntradaRepository repository) {
+	        this.repository = repository;
+	    }
+
+	    @PostMapping("/movimentoentrada/add")
+	    MovimentoEntrada addMovimentoEntrada(@RequestBody MovimentoEntrada movEntrada) {
+	        return repository.save(movEntrada);
+	    }
+
+	    @GetMapping("/movimentoentrada/getById/{id}")
+	    MovimentoEntrada getMovimentoEntradaById(@PathVariable Long id) {
+	        return repository.findById(id);
+	    }
+
+	    @GetMapping("movimentoentrada/deleteById/{id}")
+	    Boolean deleteMovimentoEntradaById(@PathVariable Long id) {
+	        return repository.delete(id);
+	    }
+
+	    @PutMapping("movimentoentrada/updateById/{id}")
+	    MovimentoEntrada updateMovimentoEntradaById(@RequestBody MovimentoEntrada fornecedor, @PathVariable Long id) {
+	        return repository.findById(id);
+	    }
+}
